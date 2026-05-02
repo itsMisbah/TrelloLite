c4c7d8bc9f633124705c48943a2d80120ae39183
itsMisbah <misbah77777@gmail.com>
2026-05-02 22:40:54 +0500

    update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ pip install -r requirements.txt
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-echo "Running migrations..."
-python manage.py migrate
+# echo "Running migrations..."
+# python manage.py migrate
 
 echo "Creating superuser if none exists..."
 python manage.py create_superuser_if_none
